454994e62f556b0c0ded4b29842348379a4cd7bb
indjar <indre.bacvinkaite@gmail.com>
2022-02-12 17:58:15 +0200

    login/register validations

--- server/src/routes/v1/users.js
+++ server/src/routes/v1/users.js
@@ -3,66 +3,79 @@ import { hash, compare } from "bcrypt";
 import jwt from "jsonwebtoken";
 import Users from "../../modules/Users.js";
 import { config } from "dotenv";
+import { body } from "express-validator";
+import { validateErrorsMiddleware } from "../../middleware/validateErrors.js";
 
 config();
 
 const router = Router();
 
-router.post("/register", async (req, res) => {
-    const { email, password } = req.body;
-    const hashed = await hash(password, 10);
+router.post("/register",
+    body(["email"]).isEmail(),
+    body(["password"]).isLength(8),
+    validateErrorsMiddleware,
+    async (req, res) => {
+        const { email, password } = req.body;
+        const hashed = await hash(password, 10);
 
-    try {
-        const user = await Users.create({ email, password: hashed });
+        try {
+            const user = await Users.create({ email, password: hashed });
 
-        res.status(200).send({
-            user
-        });
-    } catch (e) {
-        res.status(500).send({
-            error: e.message,
-        });
-    }
-});
-
-router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await Users.login(email);
-
-        const error = "Couldn't login";
-
-        if (!user) {
-            return res.status(403).send({
-                error,
+            res.status(200).send({
+                user
+            });
+        } catch (error) {
+            if (error.msg==="Invalid value"){
+                res.status(402).send(`Invalid ${param}`)
+            }
+            res.status(500).send({
+                error: error.message,
             });
         }
+    });
 
-        const isValidPw = await compare(password, user.password);
+router.post("/login",
+    body(["email"]).isEmail(),
+    body(["password"]).isLength(8),
+    validateErrorsMiddleware,
+    async (req, res) => {
+        const { email, password } = req.body;
 
-        if (!isValidPw) {
-            return res.status(403).send({
-                error,
+        try {
+            const user = await Users.login(email);
+
+            const error = "Couldn't login";
+
+            if (!user) {
+                return res.status(403).send({
+                    error,
+                });
+            }
+
+            const isValidPw = await compare(password, user.password);
+
+            if (!isValidPw) {
+                return res.status(403).send({
+                    error,
+                });
+            }
+
+            const token = jwt.sign(
+                {
+                    userId: user.id,
+                    email: user.email
+                },
+                process.env.TOKEN_SECRET
+            );
+
+            res.send({
+                token,
+            });
+        } catch (error) {
+            res.status(500).send({
+                error: error.message,
             });
         }
-
-        const token = jwt.sign(
-            {
-                userId: user.id,
-                email: user.email
-            },
-            process.env.TOKEN_SECRET
-        );
-
-        res.send({
-            token,
-        });
-    } catch (e) {
-        res.status(500).send({
-            error: e.message,
-        });
-    }
-});
+    });
 
 export default router;
